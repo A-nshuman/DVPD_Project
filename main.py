2966fa86519410134904bdf5b8368381eff50848
A-nshuman <abtbbruv@gmail.com>
2025-09-30 12:21:28 +0530

    fixing

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("nflx_2014_2023.csv")
+df = pd.read_csv('nflx_2014_2023.csv',encoding='latin1')
 
-plt.figure(figsize=(15,))
+plt.figure(figsize=(15,10))
+sns.lineplot(data=df, x='date',y='close')
+
+plt.show()
