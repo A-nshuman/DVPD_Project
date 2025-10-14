9e4436b250b7cb0aa283d8f08e0fd1f898a5ebbb
Abhishek Bhatti <abhishekbhatti009@gmail.com>
2025-10-14 12:26:04 +0530

    basic Linear Regression Example

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv('nflx_2014_2023.csv',encoding='latin1')
+plt.figure(figsize=(12,8))
 
-plt.figure(figsize=(15,10))
-sns.lineplot(data=df, x='date',y='close')
+def fitLM(xcol, ycol):
+    x = df[[xcol]]
+    y = df[ycol]
+    model = LinearRegression()
+    model.fit(x, y)
+    r2 = model.score(x,y)
+    return model, r2
 
-plt.show()
+LM, r2 = fitLM('ema_100', 'close')
+
+print(f'R2 Fit Parameter: {r2}')
+print(f'y = {LM.coef_[0]}x + {LM.intercept_}')
